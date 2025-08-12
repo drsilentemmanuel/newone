cbc846da5cbb2e3aaa3a9639e3e5e4a893e58b38
Silent Emmanuel <dzikitisilent@gmail.com>
2025-08-12 14:14:08 +0000

    home page, i am not seeing the changes

--- src/components/trust-stamp-logo.tsx
+++ src/components/trust-stamp-logo.tsx
@@ -1,6 +1,8 @@
 import { cn } from "@/lib/utils";
 import Nk7FSA01 from './svg/Nk7FSA01.svg';
 
-export const TrustStampLogo = ({ className, ...props }: React.ComponentProps<"img">) => (
-  <Nk7FSA01 className={cn("h-8 w-8 text-primary", className)} {...props} />
+export const TrustStampLogo = ({ className, ...props }: React.ComponentProps<"div">) => (
+  <div className={cn("h-8 w-8 text-primary", className)} {...props}>
+    <Nk7FSA01 />
+  </div>
 );
