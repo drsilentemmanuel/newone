999a31772c2b78558cedfb16053b41ffbe3c14ba
Silent Emmanuel <dzikitisilent@gmail.com>
2025-08-18 00:57:40 +0000

    we only have youtube, linkidin and facebook plus mobile applications

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -12,28 +12,6 @@ const WhatsAppIcon = () => (
     </svg>
 );
 
-const YouTubeIcon = () => (
-    <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="currentColor">
-        <title>YouTube</title>
-        <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"/>
-    </svg>
-);
-
-const TikTokIcon = () => (
-    <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="currentColor">
-        <title>TikTok</title>
-        <path d="M12.525.02c1.31-.02 2.61-.01 3.91-.02.08 1.53.63 3.09 1.75 4.17 1.12 1.11 2.7 1.62 4.24 1.79v4.03c-1.44-.05-2.89-.35-4.2-.97-.57-.26-1.1-.59-1.62-.93-.01 2.92.01 5.84-.02 8.75-.08 1.4-.54 2.79-1.35 3.94-1.31 1.92-3.58 3.17-5.91 3.21-2.43.05-4.84-.95-6.43-2.8-1.59-1.87-2.32-4.2-1.86-6.45.54-2.66 2.75-4.72 5.5-5.37.58-.14 1.16-.22 1.75-.28v4.02c-1.14.02-2.28.05-3.42.08.06.87.24 1.73.54 2.54.51 1.38 1.66 2.49 3.04 2.96.95.31 1.95.37 2.92.2.85-.14 1.67-.47 2.39-.94.01-1.92.01-3.84.01-5.76-.01-1.19-.42-2.34-1.12-3.29-.81-1.09-2.02-1.74-3.34-1.91v-4.15c.44.02.88.04 1.32.06z" />
-    </svg>
-)
-
-const XIcon = () => (
-     <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="currentColor">
-        <title>X</title>
-        <path d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.931ZM17.61 20.644h2.039L6.486 3.24H4.298Z"/>
-    </svg>
-)
-
-
 const AppStoreButton = () => (
     <a href="#" className="inline-block">
         <img src="https://developer.apple.com/assets/elements/badges/download-on-the-app-store.svg" alt="Download on the App Store" className="h-10"/>
@@ -109,12 +87,6 @@ export function Footer() {
 
           <div className="space-y-4 flex flex-col items-center sm:items-start col-span-full md:col-span-1">
              <div className="flex justify-center sm:justify-start space-x-4">
-                  <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer" className="hover:text-primary-foreground/80 transition-colors">
-                    <Instagram className="h-5 w-5" />
-                  </a>
-                  <a href="#" target="_blank" rel="noopener noreferrer" className="hover:text-primary-foreground/80 transition-colors">
-                     <TikTokIcon />
-                  </a>
                   <a href="https://www.linkedin.com/company/101651553/admin/dashboard/" target="_blank" rel="noopener noreferrer" className="hover:text-primary-foreground/80 transition-colors">
                     <Linkedin className="h-5 w-5" />
                   </a>
@@ -123,9 +95,6 @@ export function Footer() {
                   </a>
                   <a href="https://www.youtube.com/@TrustStamp" target="_blank" rel="noopener noreferrer" className="hover:text-primary-foreground/80 transition-colors">
                     <Youtube className="h-5 w-5" />
-                  </a>
-                   <a href="#" target="_blank" rel="noopener noreferrer" className="hover:text-primary-foreground/80 transition-colors">
-                    <XIcon />
                   </a>
               </div>
               <div className="flex flex-col space-y-4 items-center sm:items-start">
