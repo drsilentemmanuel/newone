b7fdfd157b404214390f8a1874d369d6fc49b1b0
Silent Emmanuel <dzikitisilent@gmail.com>
2025-08-28 13:38:55 +0000

    Just put an favicon related to houses

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export const metadata: Metadata = {
   manifest: '/manifest.json',
   metadataBase: new URL(siteConfig.url),
   icons: {
-    icon: '/new logo.png',
+    icon: `data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🏠</text></svg>`,
   },
   openGraph: {
     type: "website",
